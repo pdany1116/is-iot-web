ffd62b195f7c45c178a4b4f52d7fa164e8bc2d52
Popa Daniel <danypdaniel@gmail.com>
2022-01-21 01:09:22 +0200

    Add User, UserDetails, Valve models

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace IsIoTWeb.Models
+{
+    public class User : IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        [BsonElement("authorities")]
+        public List<string> Authorities { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Valve.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace IsIoTWeb.Models
+{
+    public class Valve : IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        [BsonElement("valveId")]
+        public int ValveId { get; set; }
+
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("state")]
+        public string State { get; set; }
+
+        [BsonElement("userId")]
+        public string UserId { get; set; }
+    }
+}
