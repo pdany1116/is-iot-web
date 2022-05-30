41f8e8234b9b545412f05bc1255282bb047db3e1
Popa Daniel <danypdaniel@gmail.com>
2022-05-31 01:11:03 +0300

    Remove unused filter variable

--- Models/Reading/ReadingFilter.cs
+++ Models/Reading/ReadingFilter.cs
@@ -3,7 +3,6 @@
     public class ReadingFilter
     {
         public string? CollectorId { get; set; }
-        public string? OneDate { get; set; }
         public string? FromDate { get; set; }
         public string? ToDate { get; set; }
         public int? PageSize { get; set; }
