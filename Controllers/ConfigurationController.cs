2706d2ebcec1dbabc498954baf643bbe3b9b0bbe
Catalin Sandu <36326404+sanducata@users.noreply.github.com>
2022-09-29 22:12:16 +0300

    Add schedule irrigation time (#2)
    * Created schedule irrigation page
    
    * Delete schedule

---
+++ Controllers/ConfigurationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IsIoTWeb.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
